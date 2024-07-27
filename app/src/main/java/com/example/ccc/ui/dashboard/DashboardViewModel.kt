9a1bb6dbb25e7c72559ea025aaba0afcab395bc3
theCalculatar <alpheusm13@gmail.com>
2024-07-27 17:17:03 +0200

    initial commit

---
+++ app/src/main/java/com/example/ccc/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ccc.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
